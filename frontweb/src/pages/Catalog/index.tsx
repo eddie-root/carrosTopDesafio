f80657eff9554aff52bf8618b44ea049d251312b
Admilson Ferreira <ferreiraad3@gmail.com>
2023-03-04 02:17:19 -0300

    beginer - car top grid xs

---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,13 @@
+
+import ProductCard from 'components/ProductCard';
+import './styles.css';
+
+const Catalog = () => {
+  return (
+    <div >
+      <ProductCard />
+    </div>
+  );
+};
+
+export default Catalog;
